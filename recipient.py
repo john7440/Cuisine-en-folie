cc5d5a98e063891016fc9495d23204b52e9c1dd1
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-12-04 15:22:43 +0100

    modifcation de la classe recipient + ajout d'une fonction describe

--- recipient.py
+++ recipient.py
@@ -1,5 +1,17 @@
+from dataclasses import dataclass
+from typing import Union
 
-class Recipient():
-    def __init__(self, name, contenance):
-        self.name = name
-        self.contenance = contenance
+from appareil import Appareil
+from ingredient import Ingredient
+
+
+@dataclass
+class Recipient:
+    name: str
+    content : Union[Ingredient, Appareil, None] = None
+
+    def desccribe(self):
+        if self.content is None:
+            return f"Recipient {self.name} est vide!"
+        else:
+            return f"Recipient {self.name} contiens: {self.content}"
