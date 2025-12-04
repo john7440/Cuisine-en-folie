9a8286da745a20412ddb6c40c756a7b19222ba8c
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-12-04 15:16:04 +0100

    amélioration des classe filles oeuf et chocolat

--- ingredient.py
+++ ingredient.py
@@ -6,10 +6,13 @@ class Ingredient(ABC):
         self.quantity = quantity
         self.unit = unit
 
+    def __str__(self):
+         return f"{self.quantity}{self.unit} de {self.name}"
+
 class Oeuf(Ingredient):
-    def __init__(self, name, quantity, unit):
-        Ingredient.__init__(self, name, quantity, unit)
+    def __init__(self, quantity: int):
+        super().__init__(name="Oeuf", quantity=quantity, unit="pièce")
 
 class Chocolat(Ingredient):
-    def __init__(self, name, quantity, unit):
-        Ingredient.__init__(self, name, quantity, unit)
+    def __init__(self,quantity: int):
+        super().__init__(name="Chocolat", quantity=quantity, unit="g")
