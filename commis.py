8bb308e79550273c64efdba57c26219ba3c4c587
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-12-04 14:18:19 +0100

    modification en class abstraite + héritage de thread

--- commis.py
+++ commis.py
@@ -1,5 +1,20 @@
 import threading
+from abc import ABC, abstractmethod
 
-class Commis(threading.Thread):
-    def __init__(self, name, quantity, unit):
-        threading.Thread.__init__(self)
+class Commis(threading.Thread, ABC):
+    """
+    Classe abstraite représentant un commis de cuisine
+    Chaque commis est un thread qui exécute une tâche spécifique
+    (battre des œufs, fondre du chocolat, etc.)
+    """
+    def __init__(self):
+        super().__init__()
+
+    @abstractmethod
+    def run(self):
+        """
+        Méthode abstraite que chaque commis doit implémenter.
+        Elle contient la logique de la tâche à exécuter.
+        """
+        pass
+
