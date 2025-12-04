543bf4a7004d2bafc47cbe818b1fa312383119af
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-12-04 14:17:40 +0100

    ajout de add_ingredient et homogenize

--- appareil.py
+++ appareil.py
@@ -1,3 +1,18 @@
-class Appareil():
-    def __init__(self, name):
-        self.name = name
+from dataclasses import dataclass, field
+from typing import List
+
+from ingredient import Ingredient
+
+@dataclass
+class Appareil:
+    name: str
+    ingredients: List[Ingredient] = field(default_factory=list)
+
+    def add_ingredient(self, ingredient: Ingredient) -> None:
+        self.ingredients.append(ingredient)
+        print(f"Ingrédient ajouté: {ingredient.quantity}{ingredient.unit} de {ingredient.name}")
+
+    def homogenize(self) -> None:
+        # On simule l'action de rendre le mélange homogène
+        print(f"Je mélange 'soigneusement' tous les ingrédients de l'appareil '{self.name}'...")
+        print("Le mélange est homogène et prêt à être utilisé !")
