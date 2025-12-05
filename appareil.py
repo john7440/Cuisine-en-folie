ad78b8f57eb1e7941f34afb67a3626b4b188887f
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-12-05 08:28:24 +0100

    modification de la classe Appareil

--- appareil.py
+++ appareil.py
@@ -1,18 +1,11 @@
 from dataclasses import dataclass, field
-from typing import List
-
-from ingredient import Ingredient
 
 @dataclass
 class Appareil:
-    name: str
-    ingredients: List[Ingredient] = field(default_factory=list)
+    """Représente un mélange d’ingrédients"""
+    def __init__(self, ingredients):
+        self.ingredients = ingredients
 
-    def add_ingredient(self, ingredient: Ingredient) -> None:
-        self.ingredients.append(ingredient)
-        print(f"Ingrédient ajouté: {ingredient.quantity}{ingredient.unit} de {ingredient.name}")
-
-    def homogenize(self) -> None:
-        # On simule l'action de rendre le mélange homogène
-        print(f"Je mélange 'soigneusement' tous les ingrédients de l'appareil '{self.name}'...")
-        print("Le mélange est homogène et prêt à être utilisé !")
+    def __str__(self):
+        noms = " + ".join(str(i) for i in self.ingredients)
+        return f"Appareil ({noms})"
