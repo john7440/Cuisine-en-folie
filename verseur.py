9a88a216fe5c001ba505949376c8ecb6f4c7c7dc
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-12-05 08:46:31 +0100

    modification du systeme de copie pour le contenue du recipient

--- verseur.py
+++ verseur.py
@@ -1,23 +1,17 @@
 import threading
 import time
+import copy
 from commis import Commis
 from recipient import Recipient
 
 class Verseur(Commis):
     """
     Commis chargé de verser le contenu d'un récipient source
-    dans un récipient cible à un certain rythme.
+    dans un récipient cible à un certain rythme
     """
     lock = threading.Lock()
 
     def __init__(self, recipient_source: Recipient, recipient_target: Recipient, portions: int, name: str, tempo: float = 1.0):
-        """
-        :param recipient_source: récipient contenant le mélange ou ingrédient
-        :param recipient_target: récipient dans lequel on verse
-        :param portions: nombre de portions à verser
-        :param name: nom du commis
-        :param tempo: temps (en secondes) entre chaque portion
-        """
         super().__init__(name)
         self.recipient_source = recipient_source
         self.recipient_target = recipient_target
@@ -25,15 +19,23 @@ class Verseur(Commis):
         self.tempo = tempo
 
     def run(self):
-
         with Verseur.lock:
             for i in range(1, self.portions + 1):
                 print(
-                    f"{self.name}: je verse la portion {i}/{self.portions} de '{self.recipient_source.name}' vers '{self.recipient_target.name}'")
+                    f"{self.name}: je verse la portion {i}/{self.portions} de '{self.recipient_source.name}' vers '{self.recipient_target.name}'"
+                )
                 time.sleep(self.tempo)
 
-            transferred_content = self.recipient_source.content
-            self.recipient_target.content = transferred_content
+            #on fait une copie du contenu
+            transferred_content = copy.deepcopy(self.recipient_source.content)
+
+            # fusion automatique
+            if transferred_content is not None:
+                self.recipient_target.add_content(transferred_content)
+
             self.recipient_source.content = None
+
             print(
-                f"{self.name}: transfert terminé!")
+                f"{self.name}: transfert terminé, '{self.recipient_target.name}' contient désormais {self.recipient_target.describe()}"
+            )
+
