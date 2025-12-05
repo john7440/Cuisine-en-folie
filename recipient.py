50e170c9b367c49a46a704ac852b3c55b5a9125a
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-12-05 08:30:21 +0100

    ajout d'un fonction add_content pour 'fusionner' les ingredients

--- recipient.py
+++ recipient.py
@@ -1,17 +1,27 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Union
 
 from appareil import Appareil
 from ingredient import Ingredient
 
-
 @dataclass
 class Recipient:
     name: str
-    content : Union[Ingredient, Appareil, None] = None
+    content: Union[Ingredient, Appareil, None] = None
 
-    def desccribe(self):
+    def describe(self):
         if self.content is None:
             return f"Recipient {self.name} est vide!"
         else:
-            return f"Recipient {self.name} contiens: {self.content}"
+            return f"Recipient {self.name} contient: {self.content}"
+
+    def add_content(self, ingredient: Ingredient):
+        #On ajoute un ingrédient en fusionnant si nécessaire
+        if self.content is None:
+            self.content = ingredient
+        elif isinstance(self.content, Ingredient):
+            self.content = self.content.merge(ingredient)
+        elif isinstance(self.content, Appareil):
+            self.content.ingredients.append(ingredient)
+        else:
+            self.content = ingredient
