7ad280a4e27dd656d5174f6c0d5dc0cd9dc56fe0
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-12-05 07:45:50 +0100

    ajout de 2 méthode pour ajouter la quantiter et possibilité de fusionner les ingredients

--- ingredient.py
+++ ingredient.py
@@ -1,5 +1,7 @@
 from abc import ABC
 
+from appareil import Appareil
+
 class Ingredient(ABC):
     def __init__(self, name, quantity, unit):
         self.name = name
@@ -7,12 +9,25 @@ class Ingredient(ABC):
         self.unit = unit
 
     def __str__(self):
-         return f"{self.quantity}{self.unit} de {self.name}"
+        return f"{self.quantity}{self.unit} de {self.name}"
+
+    def add_quantity(self, amount: float):
+        self.quantity += amount
+
+    def merge(self, other: "Ingredient"):
+        """Fusionne avec un autre ingrédient."""
+        if type(self) == type(other):
+            self.add_quantity(other.quantity)
+            return self
+        else:
+            return Appareil([self, other])
+
 
 class Oeuf(Ingredient):
     def __init__(self, quantity: int):
         super().__init__(name="Oeuf", quantity=quantity, unit="pièce")
 
+
 class Chocolat(Ingredient):
-    def __init__(self,quantity: int):
+    def __init__(self, quantity: int):
         super().__init__(name="Chocolat", quantity=quantity, unit="g")
